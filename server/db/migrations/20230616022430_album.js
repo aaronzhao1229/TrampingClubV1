4b17190b8f29d97259c9e375e84310c4f47201cc
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2023-06-16 15:46:04 +1200

    configure migration and seed the database

---
+++ server/db/migrations/20230616022430_album.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('album', (table) => {
+    table.increments('albumId')
+    table.string('albumName')
+    table.date('tripDate')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('album')
+}
---
+++ server/db/migrations/20230616024007_photos.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('photos', (table) => {
+    table.increments('photoId').primary()
+    table.string('photoUrl')
+    table.integer('albumId').unsigned()
+
+    table.foreign('albumId').references('album.albumId')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('photos')
+}
