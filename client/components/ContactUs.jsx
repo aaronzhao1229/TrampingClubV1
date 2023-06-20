d162077debe2f7603cf838407fc3fc8ee2e797c4
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2023-06-20 12:59:34 +1200

    add basic contact us form validation

--- client/components/ContactUs.jsx
+++ client/components/ContactUs.jsx
@@ -7,26 +7,75 @@ export default function ContactUs() {
   const [form, setForm] = useState({})
   const [errors, setErrors] = useState({})
 
-    function handleChange(event) {
-      setForm({ ...form, [event.target.name]: event.target.value })
+  function handleChange(field, value) {
+    setForm({ ...form, [field]: value })
+    // Check and see if errors exist, and remove them from the error object:
+    if (!errors[field])
+      setErrors({
+        ...errors,
+        [field]: null,
+      })
+  }
+
+  function findFormErrors() {
+    const { name, email } = form
+    const newErrors = {}
+    // name errors
+    if (!name || name === '') newErrors.name = 'Name is required'
+    // food errors
+    if (!email || email === '') newErrors.email = 'Email is required'
+    // rating errors
+    return newErrors
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    // get our new errors
+    const newErrors = findFormErrors()
+    // Conditional logic:
+    if (Object.keys(newErrors).length > 0) {
+      // We got errors!
+      setErrors(newErrors)
+    } else {
+      // No errors! Put any logic here for the form submission!
+      alert('Thank you for your feedback!')
     }
-  
+  }
 
   return (
-    <Container as="paper">
+    <Container>
       <h3>Contact us</h3>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="name">
-          <Form.Label>Your name</Form.Label>
-          <Form.Control type="text" />
+          <Form.Label>Your name (required)</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={(e) => handleChange('name', e.target.value)}
+            isInvalid={!!errors.name}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.name}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Label>Email address (required)</Form.Label>
+          <Form.Control
+            type="email"
+            onChange={(e) => handleChange('email', e.target.value)}
+            placeholder="name@example.com"
+            isInvalid={!!errors.email}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.email}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control
+            as="textarea"
+            rows={3}
+            onChange={(e) => handleChange('message', e.target.value)}
+          />
         </Form.Group>
         <Button type="submit" style={{ marginTop: '2rem' }}>
           Submit Form
