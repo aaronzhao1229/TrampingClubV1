c09eb2e0754d1cfbe5dc1ba2d4e581449a40b892
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2023-07-30 12:57:13 +0800

    finish userDb test

--- server/db/__test__/userDb.test.js
+++ server/db/__test__/userDb.test.js
@@ -61,3 +61,54 @@ describe('create a user', () => {
     expect(res[1].username).toBe('testNew')
   })
 })
+
+describe('save token', () => {
+  it('save token', async () => {
+    const username = 'test1234'
+    const token = 'qwerty'
+    await saveToken(username, token, testDb)
+    const res = await getUsers(testDb)
+    expect(res[0].refreshToken).toBe('qwerty')
+  })
+})
+
+describe('deleteToken', () => {
+  it('delete token', async () => {
+    const username = 'test1234'
+    await deleteToken(username, testDb)
+    const res = await getUsers(testDb)
+    expect(res[0].refreshToken).toBeNull()
+  })
+})
+
+describe('save reset password token', () => {
+  it('save reset password token', async () => {
+    const email = 'test@test.email'
+    const resetPasswordToken = 'zxcvbn'
+    await saveResetPasswordToken(email, resetPasswordToken, testDb)
+    const res = await getUsers(testDb)
+    expect(res[0].resetPasswordToken).toBe('zxcvbn')
+  })
+})
+
+describe('find reset password token by email', () => {
+  it('find reset password token by email', async () => {
+    const email = 'test@test.email'
+    const resetPasswordToken = 'zxcvbn'
+    await saveResetPasswordToken(email, resetPasswordToken, testDb)
+    await findResetTokenByEmail(email, testDb)
+    const res = await getUsers(testDb)
+    expect(res[0].resetPasswordToken).toBe('zxcvbn')
+  })
+})
+
+describe('reset password', () => {
+  it('reset password', async () => {
+    const email = 'test@test.email'
+    const password = 'asdfgh'
+    await resetPassword(email, password, testDb)
+
+    const res = await getUsers(testDb)
+    expect(res[0].password).toBe('asdfgh')
+  })
+})
